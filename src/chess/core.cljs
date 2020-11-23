538581f9b034fd48b170be3a068985f982129c16
Devon Zuegel <devonzuegel@gmail.com>
2020-11-22 23:28:59 -0800

    Implement naive piece movement

--- src/chess/core.cljs
+++ src/chess/core.cljs
@@ -12,25 +12,33 @@
    {:selected-piece nil
     :board [["rook", "knight", "bishop", "king", "queen", "bishop", "knight", "rook"]
             ["pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn"]
-            ["-", "-", "-", "-", "-", "-", "-", "-"]
-            ["-", "-", "-", "-", "-", "-", "-", "-"]
-            ["-", "-", "-", "-", "-", "-", "-", "-"]
-            ["-", "-", "-", "-", "-", "-", "-", "-"]
+            [nil, nil, nil, nil, nil, nil, nil, nil]
+            [nil, nil, nil, nil, nil, nil, nil, nil]
+            [nil, nil, nil, nil, nil, nil, nil, nil]
+            [nil, nil, nil, nil, nil, nil, nil, nil]
             ["pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn"]
             ["rook", "knight", "bishop", "king", "queen", "bishop", "knight", "rook"]]}))
 
 ; (defn inc-counter [current-app-state] (update-in current-app-state [:counter] inc)) ;; No need to deref it
 (defn update-selected-piece [current-app-state x y] (assoc-in current-app-state [:selected-piece] [x y])) ;; No need to deref it
+
 (defn clear-selected-piece [current-app-state] (assoc-in current-app-state [:selected-piece] nil)) ;; No need to deref it
+
+(defn get-current-selected-piece [current-app-state] (get-in current-app-state (concat [:board] (:selected-piece current-app-state))))
+; (defn get-current-selected-piece [] [:board @app-state (:selected-piece @app-state)])
 
 (defn render-piece [x y piece]
   [:td
-   {:on-click (fn [e]
+   {:key y
+    :on-click (fn [e]
                 (if (= (:selected-piece @app-state) nil)
-                  (do ; Pick up the piece
+                  (do ; Pick up the piece at the x,y coordinate
+                    (print "Pick up the piece")
                     (swap! app-state (fn [app-state] (update-selected-piece app-state x y))))
-                  (do ; Place the piece
-                    (print "Moving the piece that was at " x y " to _ _" (mod 5 2))
+                  (do ; Place the piece in the new x,y coordinates
+                    (print "Moving the piece that was at " x y " to _ _")
+                    (swap! app-state (fn [app-state] (assoc-in app-state [:board x y] (get-current-selected-piece app-state))))
+                    (swap! app-state (fn [app-state] (assoc-in app-state (concat [:board] (:selected-piece app-state)) nil)))
                     (swap! app-state (fn [app-state] (clear-selected-piece app-state))))))}
    x " " y " " piece])
 
@@ -38,16 +46,17 @@
   [:div "selected-piece: " (let [[_x _y] (:selected-piece state)] (str _x ", " _y))])
 
 (defn render-row [x row]
-  [:tr (for [[y piece] (map-indexed vector row)] (render-piece x y piece))])
+  [:tr {:key x} (for [[y piece] (map-indexed vector row)] (render-piece x y piece))])
 
 (rum/defc hello-world < rum/reactive
   []
   (let [state (rum/react app-state)] ; Register a listener to tell this component to react to the state.
     [:div
      (show-selected-piece state)
-     [:table
-      (for [[x row] (map-indexed vector (:board state))]
-        (render-row x row))]]))
+     [:pre "app state: " (pr-str @app-state)]
+     [:table [:tbody
+              (for [[x row] (map-indexed vector (:board state))]
+                (render-row x row))]]]))
 
 (rum/mount (hello-world)
            (. js/document (getElementById "app"))) ;; Here's how you use JS's dot operator
