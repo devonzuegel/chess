124564646ecebe7d5c6efb2cda9186d3b52723c4
Devon Zuegel <devonzuegel@gmail.com>
2020-11-22 22:55:20 -0800

    Select x,y on click

--- src/chess/core.cljs
+++ src/chess/core.cljs
@@ -9,7 +9,8 @@
 
 (defonce app-state
   (atom
-   {:board [["rook", "knight", "bishop", "king", "queen", "bishop", "knight", "rook"]
+   {:selected-piece [nil nil]
+    :board [["rook", "knight", "bishop", "king", "queen", "bishop", "knight", "rook"]
             ["pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn"]
             ["-", "-", "-", "-", "-", "-", "-", "-"]
             ["-", "-", "-", "-", "-", "-", "-", "-"]
@@ -18,19 +19,28 @@
             ["pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn", "pawn"]
             ["rook", "knight", "bishop", "king", "queen", "bishop", "knight", "rook"]]}))
 
-(defn inc-counter [current-app-state] (update-in current-app-state [:counter] inc)) ;; No need to deref it
+; (defn inc-counter [current-app-state] (update-in current-app-state [:counter] inc)) ;; No need to deref it
+(defn update-selected-piece [current-app-state x y] (assoc-in current-app-state [:selected-piece] [x y])) ;; No need to deref it
 
-(defn render-piece [x y piece] [:td {:on-click (fn [e] (print e))} x " " y " " piece])
+(defn render-piece [x y piece]
+  [:td
+   {:on-click (fn [e] (swap! app-state (fn [app-state] (update-selected-piece app-state x y))))}
+   x " " y " " piece])
+
+(defn show-selected-piece [state]
+  [:div "selected-piece: " (let [[_x _y] (:selected-piece state)] (str _x ", " _y))])
+
+(defn render-row [x row]
+  [:tr (for [[y piece] (map-indexed vector row)] (render-piece x y piece))])
 
 (rum/defc hello-world < rum/reactive
   []
-  (let [state (rum/react app-state)] ;; Register a listener to tell this component to react to the state.
+  (let [state (rum/react app-state)] ; Register a listener to tell this component to react to the state.
     [:div
+     (show-selected-piece state)
      [:table
       (for [[x row] (map-indexed vector (:board state))]
-        [:tr
-         (for [[y piece] (map-indexed vector row)] (render-piece x y piece))
-         ])]]))
+               (render-row x row))]]))
 
 (rum/mount (hello-world)
            (. js/document (getElementById "app"))) ;; Here's how you use JS's dot operator
